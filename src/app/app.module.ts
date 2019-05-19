3d34cf7c10c7a9a7df037dee61ea8cc9766429a4
Liisa Duerig-Laitinen <lkduerig@gmail.com>
2019-05-19 14:09:39 +0300

    Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'; // TODO Can this be combined with previous line?
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { HelloComponent } from './hello.component';
 import { NoteComponent } from './note/note.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, NoopAnimationsModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, NoteComponent ],
   bootstrap:    [ AppComponent ]
 })
